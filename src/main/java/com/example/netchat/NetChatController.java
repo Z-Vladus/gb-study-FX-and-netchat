4eea377ba3ebb9e048275e3b0e3f9b790df56c9d
Z-vladus <z-vladus@bk.ru>
2022-04-13 21:01:34 +0300

    Lesson7 Homework prepare

--- src/main/java/com/example/netchat/NetChatController.java
+++ src/main/java/com/example/netchat/NetChatController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 public class NetChatController {
     @FXML
@@ -15,9 +18,73 @@ public class NetChatController {
     private TextField messageField;
 
 
+
+
+
     public void sendButtonClick(ActionEvent actionEvent) {
         System.out.println("sendbtnclick");
         messageArea.insertText(0, "["+ LocalDateTime.now() +"] " + messageField.getText() + "\n");
+        Map<String,String> map = new Map<String, String>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public String get(Object key) {
+                return null;
+            }
+
+            @Override
+            public String put(String key, String value) {
+                return null;
+            }
+
+            @Override
+            public String remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends String, ? extends String> m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<String> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<String> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<String, String>> entrySet() {
+                return null;
+            }
+        };
 
     }
 
